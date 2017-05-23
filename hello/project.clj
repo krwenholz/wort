5c4bb29c2b47ed0a8946b65f0c14fc6430c553c3
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-05-23 02:52:41 +0000

    master: previous version not stable

--- hello/project.clj
+++ hello/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.7"]
+                 [org.clojure/data.json "0.2.6"]
                  [com.amazonaws/aws-lambda-java-core "1.0.0"]]
   :main ^:skip-aot hello.core
   :target-path "target/%s"
