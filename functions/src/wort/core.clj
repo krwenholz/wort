c0d95511dfdf464804c5f165df97bc88206d6d2a
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-05-27 13:52:46 +0000

    dat_contribution: transforming payload data for easier manipulation

--- functions/src/wort/core.clj
+++ functions/src/wort/core.clj
@@ -2,6 +2,13 @@
   (:gen-class
    :methods [^:static [handler [String] String]]))
 
+(require '[clojure.string :as str])
+
 (defn -handler
   [s]
   (str "Wort " s " ツ"))
+
+(defn get_key_as_list
+  [payload key]
+  (str/split (get payload key) #"\s|[\.\,\!\?]")
+)
