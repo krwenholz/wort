302d5969b8ef1b2ead41479e59ff7494d1c128dd
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-06-03 22:25:07 +0000

    dat_contribution: simplified regex, more robust testing

--- functions/test/wort/core_test.clj
+++ functions/test/wort/core_test.clj
@@ -6,11 +6,21 @@
   (testing "FIXME, I fail."
     (is (= 1 1))))
 
-(deftest _get_words_from_key
+(deftest test_get_words_from_key
   (testing "splits on various permutations"
     (is (= ["a" "test"] (get_words_from_key {"key" "a test"} "key" )))
+
+    (is (= [] (get_words_from_key {"key" ""} "key" )))
+
     (is (= ["a" "test"] (get_words_from_key {"key" "  a  test  "} "key" )))
+
     (is (= ["a" "test"] (get_words_from_key {"key" "a test!"} "key" )))
+
     (is (= ["a" "test"] (get_words_from_key {"key" "a...test!"} "key" )))
+
+    (is (= ["OMG" "Sally" "is" "like" "all" "about" "that" "money" "Maybe" "or" "may" "not" "be" "true"]
+           (get_words_from_key {"key" "OMG!!!! Sally, is, like, all about that $money,,... *Maybe or may not be true"} "key" )))
+
     (is (= ["a" "test"] (get_words_from_key {"key" " !!!!a...!!!!!test! "} "key" )))
+
     (is (= ["This" "is" "a" "test"] (get_words_from_key {"key" "This, is a test!"} "key" )))))
