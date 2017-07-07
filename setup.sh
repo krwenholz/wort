4de94e646e409aaee41d9933b1931d2192e454a2
Kyle R Wenholz <kyle@krwenholz.com>
2017-07-06 23:06:13 -0700

    Make setup create not update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # Install the AWS CLI and run this script
 # ./setup.sh GITHUB_USER GITHUB_TOKEN GITHUB_REPO BRANCH_NAME
-aws cloudformation update-stack --stack-name=bobdylan --template-body "$(cat pipeline-definition.yaml)" --region=us-east-1 --capabilities=CAPABILITY_IAM --parameters ParameterKey=GitHubUser,ParameterValue=$1 ParameterKey=GitHubToken,ParameterValue=$2 ParameterKey=Repo,ParameterValue=$3 ParameterKey=Branch,ParameterValue=$4
+aws cloudformation create-stack --stack-name=bobdylan --template-body "$(cat pipeline-definition.yaml)" --region=us-east-1 --capabilities=CAPABILITY_IAM --parameters ParameterKey=GitHubUser,ParameterValue=$1 ParameterKey=GitHubToken,ParameterValue=$2 ParameterKey=Repo,ParameterValue=$3 ParameterKey=Branch,ParameterValue=$4
