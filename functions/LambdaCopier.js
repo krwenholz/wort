6ae53d77d25a7c0ca76b438bb563d772e42713da
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-08-15 02:14:09 +0000

    front-end:

--- functions/LambdaCopier.js
+++ functions/LambdaCopier.js
@@ -1,36 +1,36 @@
 exports.handler = function(event, context) {
-    var buckets = {};
-
-    buckets.in = {
-        "Bucket":"central-data-repository",
-        "Key":"sandbox" + "/" + event.ResourceProperties.InputFile
-    };
-
-    buckets.out = {
-        "Bucket":"sandbox-data",
-        "Key":event.ResourceProperties.OutputFile || event.ResourceProperties.InputFile
-    };
-
     var AWS = require('aws-sdk');
     var S3 = new AWS.S3();
+
+    var buckets = {
+        in: {
+            "Bucket": event.ResourceProperties.S3BucketInput,
+            "Key": event.ResourceProperties.S3KeyInput,
+        },
+        out: {
+            "Bucket": event.ResourceProperties.WebsiteBucketName,
+            "Key":event.ResourceProperties.S3BucketDestination,
+        }
+    };
 
     S3.getObject(buckets.in, function(err, data) {
         if (err) {
             console.log("Couldn't get file " + buckets.in.Key);
             context.fail("Error getting file: " + err);
+            return;
         }
-        else {
-            buckets.out.Body = data.Body;
-            S3.putObject(buckets.out, function(err, data) {
-                if (err) {
-                    console.log("Couln't write to S3 bucket " + buckets.out.Bucket);
-                    context.fail("Error writing file: " + err);
-                }
-                else {
-                    console.log("Successfully copied " + buckets.in.Key + " to " + buckets.out.Bucket + " at " + buckets.out.Key);
-                    context.succeed();
-                }
-            });
-        }
+
+        buckets.out.Body = data.Body;
+
+        S3.putObject(buckets.out, function(err, data) {
+            if (err) {
+                console.log("Couln't write to S3 bucket " + buckets.out.Bucket);
+                context.fail("Error writing file: " + err);
+            }
+            else {
+                console.log("Successfully copied " + buckets.in.Key + " to " + buckets.out.Bucket + " at " + buckets.out.Key);
+                context.succeed();
+            }
+        });
     });
 };
--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 # Install the AWS CLI and run this script
 # ./setup.sh GITHUB_USER GITHUB_TOKEN GITHUB_REPO BRANCH_NAME
-aws cloudformation create-stack --stack-name=bobdylan --template-body "$(cat pipeline-definition.yaml)" --region=us-east-1 --capabilities=CAPABILITY_IAM --parameters ParameterKey=GitHubUser,ParameterValue=$1 ParameterKey=GitHubToken,ParameterValue=$2 ParameterKey=Repo,ParameterValue=$3 ParameterKey=Branch,ParameterValue=$4
+
+aws cloudformation create-stack \
+    --stack-name=bobdylan \
+    --template-body "$(cat pipeline-definition.yaml)" \
+    --region=us-east-1 \
+    --capabilities=CAPABILITY_IAM \
+    --parameters \
+    ParameterKey=GitHubUser,ParameterValue=$1 \
+    ParameterKey=GitHubToken,ParameterValue=$2 \
+    ParameterKey=Repo,ParameterValue=$3 \
+    ParameterKey=Branch,ParameterValue=$4
