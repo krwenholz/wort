1afc51046b7fdc296fcc91255fc1c2b031c19fc3
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-09-18 20:05:52 -0600

    Add wip sass to css compiler task

--- functions/project.clj
+++ functions/project.clj
@@ -9,12 +9,17 @@
                  [uswitch/lambada "0.1.2"]
                  [pjstadig/humane-test-output "0.8.2"]
                  [compojure "1.6.0"]
+                 [deraen/sass4clj "0.3.1"]
                  [ring/ring-defaults "0.2.1"]]
+  :sass4clj {:source-paths ["resources/public/css/scss"]
+             :target-path "resources/public/css"
+             :output-style :compressed}
   :ring {:handler wort.handler/app}
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
   :plugins [[lein-cljfmt "0.5.6"]
-	    [lein-ring "0.12.1"]]
+	        [lein-ring "0.12.1"]
+            [deraen/lein-sass4clj "0.3.1"]]
   :main ^:skip-aot wort.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
